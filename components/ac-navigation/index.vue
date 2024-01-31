a4487f6a58e0b8a76f7ae02474bbffa52601c2d5
Marco Dell'Anna <marco.dellanna@dmgroup.it>
2024-01-31 12:37:36 +0100

    fix: toggler

--- components/ac-navigation/index.vue
+++ components/ac-navigation/index.vue
@@ -7,10 +7,10 @@
             academy
           </span>
         </a>
-        <button ref="hamburger" class="inline-flex items-center justify-center border p-2 text-neutral-400 hover:text-black focus:text-black focus:outline-none md:hidden" @click="toggleMenu()">
+        <button ref="hamburger" class="inline-flex items-center justify-center p-2 text-neutral-400 hover:text-black focus:text-black focus:outline-none md:hidden" @click="toggleMenu()">
           <svg class="size-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-            <path :class="{ 'hidden': dropdownIsOpen, 'inline-flex': !dropdownIsOpen }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            <path :class="{ 'hidden': !dropdownIsOpen, 'inline-flex': dropdownIsOpen }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            <path :class="{ 'hidden': menuIsOpen, 'inline-flex': !menuIsOpen }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            <path :class="{ 'hidden': !menuIsOpen, 'inline-flex': menuIsOpen }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
       </div>
