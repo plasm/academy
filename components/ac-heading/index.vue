b3ca4b588b1a825671a9abbd6870bce55b971f09
Marco Dell'Anna <marco.dellanna@dmgroup.it>
2024-01-31 22:58:02 +0100

    feat: heading

---
+++ components/ac-heading/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div data-name="ac-heading" class="mx-auto max-w-3xl p-6 text-center lg:p-10">
+    <div class="text-5xl font-semibold tracking-tight text-white lg:text-7xl">
+      <slot name="title">
+        <div v-html="title" />
+      </slot>
+    </div>
+    <div class="mt-4 text-xl tracking-tight text-neutral-300 lg:text-xl">
+      <slot name="description">
+        <div v-html="description" />
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ac-heading',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="mx-auto px-4 sm:px-6 md:px-8 2xl:max-w-7xl">
+    <div class="py-10">
+      <ac-heading description="Kickstart your next project and grow your revenue with this high-converting, beautiful template that you will love.">
+        <template #title>
+          <div class="leading-tight">
+            Aiges <span class="font-light text-primary underline">Academy</span><br>Scopri, Apprendi, Eccelli.
+          </div>
+        </template>
+      </ac-heading>
+    </div>
     <div class="text-4xl text-white">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, harum ullam, velit, minima nam <span class="text-primary">accusantium</span> voluptas ducimus non quam distinctio sint nihil voluptatibus adipisci aut facilis minus natus nostrum quo?
     </div>
