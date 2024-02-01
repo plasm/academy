39d2c071eaa12f70a9d63d3467db9edeb353d9c7
Marco Dell'Anna <marco.dellanna@dmgroup.it>
2024-02-01 21:44:02 +0100

    feat: gradient

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-auto px-4 sm:px-6 md:px-8 2xl:max-w-7xl">
-    <div class="py-10">
+    <div class="gradient py-10">
       <ac-heading description="Kickstart your next project and grow your revenue with this high-converting, beautiful template that you will love.">
         <template #title>
           <div class="leading-none ">
@@ -41,4 +41,7 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.gradient{
+  background: radial-gradient(50.9% 35.5% at 50% 38%, rgba(200, 200, 255, .16) 0%, #17171700 100%);
+}</style>
