4aab0d55a199f4545271624b9f022d7aa6fcf7d1
Fabrizio Dell'Anna <fabrizio.dellanna@aiges.org>
2024-02-03 10:58:45 +0100

    feat: pages

--- pages/scuola.vue
+++ pages/scuola.vue
@@ -1,6 +1,6 @@
 <template>
   <div data-name="page-scuola" class="mx-auto px-4 sm:px-6 md:px-8 2xl:max-w-7xl">
-    <ac-heading description="Ti insegniamo le conoscenze, le abilità e le competenze necessarie per eccellere nella gestione del conflitto familiare.">
+    <ac-heading description="Ti insegniamo le conoscenze, le abilità e le competenze necessarie per padroneggiare la gestione del conflitto familiare.">
       <template #title>
         <div class="leading-none ">
           Formiamo persone,<br> creiamo specialisti
