a4d2eb79d123674683af0e88e1ee369d8aa5fe2a
Fabrizio Dell'Anna <fabrizio.dellanna@aiges.org>
2024-02-03 07:05:47 +0100

    feat: pages

--- pages/sessioni.vue
+++ pages/sessioni.vue
@@ -1,6 +1,6 @@
 <template>
   <div data-name="page-sessioni" class="mx-auto px-4 sm:px-6 md:px-8 2xl:max-w-7xl">
-    <ac-heading description="L’obiettivo è uno e uno soltanto: andare alla radice della parola “insegnare”, cioè lasciare il segno.">
+    <ac-heading description="L’obiettivo è uno e uno soltanto: andare alla radice della parola “insegnare”. In altre parole, lasciare il segno.">
       <template #title>
         <div class="leading-none">
           Creiamo eventi<br>con l’X-Factor
