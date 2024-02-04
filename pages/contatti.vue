01e230068dde850ba302c0077c1271815ced7616
Fabrizio Dell'Anna <fabrizio.dellanna@aiges.org>
2024-02-04 19:06:14 +0100

    feat: pages

--- pages/contatti.vue
+++ pages/contatti.vue
@@ -1,9 +1,9 @@
 <template>
   <div data-name="page-contatti" class="mx-auto px-4 sm:px-6 md:px-8 2xl:max-w-7xl">
-    <ac-heading description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error blanditiis earum accusamus amet minus quas asperiores!">
+    <ac-heading description="Esplora i nuovi orizzonti della carriera professionale ed entra in contatto con la tua nuova dimensione.">
       <template #title>
         <div class="leading-none ">
-          Il tuo futuro inizia<br> con un contatto
+          Il futuro ti aspetta,<br> chiama
         </div>
       </template>
     </ac-heading>
