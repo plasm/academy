0cb2671232adc2c81b5129eb5eb321f95b7d79c8
Marco Dell'Anna <marco.dellanna@dmgroup.it>
2024-02-04 10:45:40 +0100

    fix

--- components/ac-mosaic/index.vue
+++ components/ac-mosaic/index.vue
@@ -2,7 +2,7 @@
   <div data-name="ac-mosaic" class="mx-auto grid max-w-6xl grid-cols-4 gap-2">
     <div class="col-span-2  rounded-lg bg-neutral-950 " />
     <div class="aspect-square rounded-lg bg-neutral-950" />
-    <div class="gradient-primary flex aspect-square flex-col items-center justify-center gap-2 rounded-lg p-2 text-center text-neutral-900/90">
+    <div class="flex aspect-square flex-col items-center justify-center gap-2 rounded-lg bg-primary p-2 text-center text-neutral-900/90">
       <span class="text-9xl font-bold">4</span>
       <span class="text-lg">Proposte formative <b>imperdibili</b></span>
     </div>
@@ -19,11 +19,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.gradient-primary{
-  background: rgb(242,254,113);
-  background: -moz-linear-gradient(52deg, rgba(242,254,113,1) 49%, rgba(255,255,181,1) 100%);
-  background: -webkit-linear-gradient(52deg, rgba(242,254,113,1) 49%, rgba(255,255,181,1) 100%);
-  background: linear-gradient(52deg, rgba(242,254,113,1) 49%, rgba(255,255,181,1) 100%);
-  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#f2fe71",endColorstr="#ffffb5",GradientType=1);
-}
 </style>
