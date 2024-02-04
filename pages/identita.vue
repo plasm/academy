1fe32b05352103e7547620eb9e245e676fa08d2b
Fabrizio Dell'Anna <fabrizio.dellanna@aiges.org>
2024-02-04 18:42:04 +0100

    feat: pages

--- pages/identita.vue
+++ pages/identita.vue
@@ -19,3 +19,35 @@ export default {
 <style lang="scss" scoped>
 
 </style>
+
+1W (Chi siamo)
+
+Siamo l’academy per specialisti del conflitto familiare.
+
+La nostra attività è totalmente dedicata alla formazione specialistica nell’ambito delle ADR per la famiglia, con un focus specifico sulla mediazione familiare e sulla coordinazione genitoriale.
+
+Con noi, lavorano ogni giorno docenti e relatori di altissimo profilo, che hanno alle spalle anni di ricerca, studio ed esperienza, oltre a una naturale propensione per l’insegnamento di nuova generazione.
+
+2W (Cosa facciamo)
+
+Promuoviamo programmi di Master Experience, alta formazione, aggiornamento professionale e supervisione nelle discipline di riferimento, ponendoci come trendsetter nello sviluppo e nel potenziamento delle conoscenze, delle abilità e delle competenze necessarie per eccellere nella gestione delle dinamiche familiari complesse.
+
+3W (Dove ci troviamo)
+
+Ci troviamo in un luogo moderno, accogliente e di facile accesso per i professionisti desiderosi di arricchire le proprie conoscenze, abilità e competenze: siamo online, nel cuore del World Wide Web. E, di tanto in tanto, anche in presenza.
+
+4W (Quando possiamo aiutarti)
+
+Possiamo aiutarti sia nell’acquisizione di nuove competenze, sia nel perfezionamento di quelle già acquisite.
+
+5W (Perché sceglierci)
+
+Immagina che, in un triangolo, ciascun vertice rappresenti un elemento fondamentale per il successo professionale: la formazione, la tutela di categoria e il lavoro.
+
+Il primo vertice rappresenta AIGES Academy, il punto di partenza per una formazione di eccellenza, con un’ampia gamma di corsi progettati per diventare specialisti del settore.
+
+Il secondo vertice rappresenta MediaCoor, il partner affidabile per un’efficace tutela di categoria, con servizi pensati per proteggere gli interessi dei mediatori familiari e dei coordinatori genitoriali.
+
+Il terzo vertice rappresenta l’Agenzia Italiana Genitori Separati, la possibile porta di accesso al lavoro, con il supporto indispensabile per individuare opportunità in linea con le aspirazioni professionali.
+
+Insieme, questi tre vertici formano il paradigma del successo, offrendoti un ambiente arricchente, sicuro e collaborativo per perseguire con serenità tutti i tuoi obiettivi.
