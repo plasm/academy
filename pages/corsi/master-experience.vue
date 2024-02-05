87adee4ce461e41b39d33664cc33653ef19f8314
Fabrizio Dell'Anna <fabrizio.dellanna@aiges.org>
2024-02-05 13:45:02 +0100

    feat: pages

--- pages/corsi/master-experience.vue
+++ pages/corsi/master-experience.vue
@@ -10,6 +10,8 @@
   </div>
 </template>
 
+Il Master Experience è un percorso di qualifica professionale, rivolto a studenti e professionisti desiderosi di crescita personale e professionale. È strutturato in primo e secondo livello e richiede specifiche lauree e/o documentata esperienza settoriale. La durata minima è di un anno, con requisiti di ammissione variabili. Il numero di allievi è limitato, a garanzia di un insegnamento personalizzato. La selezione avviene attraverso un colloquio motivazionale online. Gli obiettivi e i programmi sono definiti dalla direzione scientifica di AIGES Academy. Le lezioni si svolgono online, in presenza e/o in modalità blended. Al fine di monitorare i progressi, oltre alla prova finale, possono essere previste verifiche intermedie. Al termine, gli allievi conseguono…
+
 <script>
 export default {
   name: 'page-master-experience'
