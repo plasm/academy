a3b8fea46e8f9f0ae00210b8e5cc43c2f27eca46
Fabrizio Dell'Anna <fabrizio.dellanna@aiges.org>
2024-02-06 17:36:50 +0100

    feat: pages

--- pages/corsi/alta-formazione.vue
+++ pages/corsi/alta-formazione.vue
@@ -7,8 +7,9 @@
         </div>
       </template>
     </ac-heading>
-
-    L’Alta formazione di AIGES Academy comprende corsi professionalizzanti, di durata compresa tra SPECIFICARE e SPECIFICARE, progettati per fornire a professionisti e laureati conoscenze, abilità e competenze avanzate in specifiche discipline. I requisiti di ammissione sono variabili. Il numero di allievi è limitato per garantire un’attenzione personalizzata e favorire un ambiente di apprendimento interattivo. I programmi sono definiti dalla direzione scientifica. Le lezioni si svolgono online, in presenza e/o in modalità blended. Non sono previste né verifiche intermedie né prova finale. Al termine dell’impegno didattico, viene rilasciato un attestato di partecipazione.
+    <div class="text-white">
+      L’Alta formazione di AIGES Academy consiste in corsi professionalizzanti, di durata compresa tra trenta e sessanta ore, progettati per fornire… a professionisti e laureati. Non sono richiesti requisiti di ammissione specifici. Il numero di allievi è limitato per garantire un’attenzione personalizzata e favorire un ambiente di apprendimento interattivo. I programmi sono definiti dalla direzione scientifica. Le lezioni si svolgono online, in presenza e/o in modalità blended. Non sono previsti esami. Al termine dell’impegno didattico, viene rilasciato un attestato.
+    </div>
   </div>
 </template>
 
--- pages/corsi/master-experience.vue
+++ pages/corsi/master-experience.vue
@@ -1,7 +1,7 @@
 <template>
   <div data-name="page-master-experience">
     <div class="mx-auto px-4 sm:px-6 md:px-8 2xl:max-w-7xl">
-      <ac-heading description="Ti conduciamo oltre i confini della formazione convenzionale per un'esperienza trasformativa senza precedenti.">
+      <ac-heading description="Ti conduciamo oltre i confini della formazione convenzionale per vivere un’esperienza trasformativa senza precedenti.">
         <template #title>
           <div class="leading-none ">
             Master <span class="text-primary">Experience</span>
@@ -9,20 +9,20 @@
         </template>
       </ac-heading>
       <div class="text-white">
-        I Master Experience di AIGES Academy sono percorsi accademici di qualifica, strutturati in primo e secondo livello, della durata minima di un anno, progettati per fornire… a professionisti e laureati. I requisiti di ammissione variano in base alla disciplina e sono soggetti a valutazione. Il numero di allievi è limitato per garantire un’attenzione personalizzata e favorire un ambiente di apprendimento interattivo. I programmi sono definiti dalla direzione scientifica. Le lezioni si svolgono online, in presenza e/o in modalità blended. È previsto un esame per ciascun livello. Il numero di attestati rilasciati varia a seconda degli obiettivi prefissati.
+        Il Master Experience di AIGES Academy consiste in percorsi accademici di qualifica, di primo e secondo livello, della durata minima di un anno, progettati per fornire… a professionisti e laureati. Sono richiesti requisiti di ammissione specifici, che sono soggetti a valutazione. Il numero di allievi è limitato per garantire un’attenzione personalizzata e favorire un ambiente di apprendimento interattivo. I programmi sono definiti dalla direzione scientifica. Le lezioni si svolgono online, in presenza e/o in modalità blended. È previsto un esame per ciascun livello. Al termine dell’impegno didattico, viene rilasciato uno o più attestati, a seconda degli obiettivi prefissati.
       </div>
     </div>
 
     <div class="mx-auto my-10 bg-neutral-800 px-4  py-10 sm:px-6 md:px-8 2xl:max-w-7xl">
       <div class=" text-center text-4xl leading-tight text-white">
-        <div class="font-semibold text-primary">
-          IL VALORE AGGIUNTO
+        <div class="font-semibold text-white">
+          I PLUS
         </div>
-        <div class="font-semibold text-4xl">
+        <div class="font-semibold text-4xl text-primary">
           SAPERE, SAPER FARE, SAPER ESSERE
         </div>
         <div class="text-xl mt-2">
-          grazie ai suoi tre elementi chiave, la formula Experience trasforma la formazione in una vera e propria esperienza.
+          Grazie ai suoi plus, il Master Experience trasforma la formazione in una vera e propria esperienza.
         </div>
       </div>
     </div>
