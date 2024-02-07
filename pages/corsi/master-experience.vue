f2093270843393ef5a93fceeff208c4f46e7d115
Marco Dell'Anna <marco.dellanna@dmgroup.it>
2024-02-07 16:56:32 +0100

    master page

--- pages/corsi/master-experience.vue
+++ pages/corsi/master-experience.vue
@@ -1,6 +1,6 @@
 <template>
   <div data-name="page-master-experience">
-    <div class="mx-auto px-4 sm:px-6 md:px-8 2xl:max-w-7xl">
+    <div class="mx-auto mt-10 px-4 sm:px-6 md:px-8 2xl:max-w-7xl">
       <ac-shield class="top-4" persistent experience theme="gold" />
       <ac-heading description="Ti conduciamo oltre i confini della formazione convenzionale per vivere un’esperienza trasformativa senza precedenti.">
         <template #title>
@@ -22,29 +22,51 @@
         </nuxt-link>
       </div>
 
+      <div class="mx-auto max-w-6xl p-20 pt-0">
+        <div class="mb-8 flex flex-col gap-4 text-2xl font-light text-white">
+          <div>Il Master Experience di <span class="text-gold">AIGES Academy</span> è un format strutturato in percorsi di qualifica professionale di durata non inferiore a un anno, progettati per professionisti e laureati interessati ad acquisire conoscenze, abilità e competenze di livello accademico.</div>
+
+          <div>L’iscrizione richiede requisiti di ammissione specifici, soggetti a valutazione. L’accesso è riservato a un massimo di trenta allievi, al fine di garantire un’attenzione personalizzata e favorire un ambiente di apprendimento interattivo.</div>
+        </div>
+      </div>
+
       <div class="mx-auto grid max-w-6xl grid-cols-2 gap-6">
-        <div class="mx-auto rounded-xl bg-gradient-to-br from-neutral-800 to-neutral-900 px-4 py-14 text-lg text-white sm:px-6 md:px-12">
-          <div class="mb-4 text-xl font-medium">
-            Il Master Experience
+        <div class="group relative mx-auto overflow-hidden rounded-xl bg-gradient-to-br from-neutral-800 to-neutral-900 text-lg text-white ">
+          <div class="absolute right-2 top-2 scale-75">
+            <ac-shield experience theme="gold" />
           </div>
-          <div class="mb-8 flex flex-col gap-4 text-base font-light text-white/70">
-            <div>Il Master Experience di <span class="text-gold">AIGES Academy</span> è un format strutturato in percorsi di qualifica professionale di durata non inferiore a un anno, progettati per professionisti e laureati interessati ad acquisire conoscenze, abilità e competenze di livello accademico.</div>
-
-            <div>L’iscrizione richiede requisiti di ammissione specifici, soggetti a valutazione. L’accesso è riservato a un massimo di trenta allievi, al fine di garantire un’attenzione personalizzata e favorire un ambiente di apprendimento interattivo.</div>
-
-            <div class="text-sm text-white/50">
-              Al termine del programma, vengono rilasciati uno o più attestati, a seconda degli obiettivi prefissati.
+          <div class="p-8 pr-32 ">
+            <div class="mb-4 text-xl font-medium">
+              Attestato e riconoscimenti
+            </div>
+            <div class="mb-8 flex flex-col gap-4 text-base font-light text-white/70">
+              <p class="mb-8 text-base font-light text-white/70 ">
+                Al termine del programma, vengono rilasciati uno o più attestati, a seconda degli obiettivi prefissati.
+              </p>
             </div>
           </div>
-        </div>
-        <div class="relative mx-auto overflow-hidden rounded-xl bg-gradient-to-br from-gold/80  to-gold/30 px-4 py-14 text-white sm:px-6 md:px-12">
-          <div class="mb-4 text-xl font-medium">
-            Online o in presenza
+
+          <div class="relative -bottom-20 left-4 -mt-32 size-full rounded-xl bg-black p-8 text-sm text-neutral-800">
+            <div class="absolute flex gap-2">
+              <div class="h-[300px] w-[240px] rounded-xl bg-neutral-200 shadow-xl transition-all duration-300 ease-cubic-bezier group-hover:-translate-x-0" />
+              <div class="h-[300px] w-[240px] -translate-x-20 rounded-xl bg-neutral-300 shadow-xl transition-all duration-300 ease-cubic-bezier group-hover:-translate-x-0" />
+              <div class="h-[300px] w-[240px] -translate-x-40 rounded-xl bg-neutral-400 shadow-xl transition-all duration-300 ease-cubic-bezier group-hover:-translate-x-0" />
+            </div>
+            <!-- <img src="https://i.ytimg.com/vi_webp/EQUoimenMAg/maxresdefault.webp"> -->
           </div>
-          <p class="mb-8 text-base font-light text-white/70 ">
-            I contenuti sono definiti dalla direzione scientifica. Le lezioni si svolgono online, in presenza e/o in modalità blended. Sono previsti esami.
-          </p>
-          <div class="absolute w-full rounded-xl bg-white p-8 text-sm text-neutral-800">
+        </div>
+
+        <div class="group relative mx-auto overflow-hidden rounded-xl bg-gradient-to-br from-gold/80  to-gold/30 text-white ">
+          <div class="p-8">
+            <div class="mb-4 text-xl font-medium">
+              Online o in presenza
+            </div>
+            <p class="mb-8 text-base font-light text-white/70 ">
+              I contenuti sono definiti dalla direzione scientifica. Le lezioni si svolgono online, in presenza e/o in modalità blended. Sono previsti esami.
+            </p>
+          </div>
+
+          <div class="relative -bottom-24 -right-20 -mt-28 w-full rounded-xl bg-white p-8 text-sm text-neutral-800 transition-transform duration-500 ease-cubic-bezier group-hover:-translate-x-4 group-hover:-translate-y-4">
             <div class="mb-2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consequuntur nesciunt ea quia vel quod.
             </div>
