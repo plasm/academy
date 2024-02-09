5ab3d85cdfbfc5cefd79647cc51d68dcb60b7c6d
Fabrizio Dell'Anna <fabrizio.dellanna@aiges.org>
2024-02-09 10:06:46 +0100

    feat: pages

--- pages/corsi/alta-formazione.vue
+++ pages/corsi/alta-formazione.vue
@@ -23,6 +23,149 @@
         </div>
       </div>
     </div>
+    <!--
+    <div class="mx-auto max-w-6xl  px-4">
+      <div class="my-10 text-center text-4xl leading-tight text-white">
+        Il <span class="text-gold">Master Experience</span> adotta un approccio olistico alla formazione, armonizzando perfettamente la conoscenza teorica, l’applicazione pratica e lo sviluppo personale.
+      </div>
+
+      <br>
+      <div class="mb-2 text-lg font-semibold  text-gold">
+        A chi è destinato
+      </div>
+      <br>
+      <div class="text-white">
+        I destinatari sono avvocati, praticanti abilitati, psichiatri, psicologi, assistenti sociali, mediatori familiari, counselor, pedagogisti, educatori professionali, criminologi, sociologi e laureati in area umanistica, sanitaria e/o sociale.
+      </div>
+      <br>
+
+      <br>
+      <div class="mb-2 text-lg font-semibold  text-gold">
+        Il programma
+      </div>
+      <br>
+      <div class="text-white">
+        Modulo 1 - Presupposti giuridici della coordinazione genitoriale
+        <br>
+        Modulo 2 - Introduzione alla coordinazione genitoriale
+        <br>
+        Modulo 3 - Presupposti metodologici della coordinazione genitoriale
+        <br>
+        Modulo 4 - Illustrazione analitica del metodo della coordinazione genitoriale
+      </div>
+      <br>
+
+      <br>
+      <div class="mb-2 text-lg font-semibold  text-gold">
+        I docenti
+      </div>
+      <br>
+      <div class="text-white">
+        Arturo Maniaci, avvocato civilista, professore associato di Istituzioni di diritto privato presso l’Università Statale di Milano.
+      </div>
+      <br>
+      <div class="text-white">
+        Romolo Donzelli, avvocato civilista e cassazionista, professore ordinario di Diritto processuale civile presso l’Università di Macerata, docente presso la Scuola di specializzazione per le professioni legali delle Università di Camerino e Macerata.
+      </div>
+      <br>
+      <div class="text-white">
+        Manuela Pagliaroli, avvocata civilista, mediatrice familiare, scolastica, interculturale, civile e commerciale, coordinatrice genitoriale, formatrice e supervisora in mediazione familiare e coordinazione genitoriale, cofondatrice e direttrice scientifica di AIGES Academy, socia fondatrice e coordinatrice del Comitato scientifico di MediaCoor.
+      </div>
+      <br>
+
+      <br>
+      <div class="mb-2 text-lg font-semibold  text-gold">
+        Il materiale didattico
+      </div>
+      <br>
+      <div class="text-white">
+        Toolkit professionale con gli strumenti di lavoro per lo svolgimento del processo di coordinazione genitoriale.
+      </div>
+      <br>
+
+      <br>
+      <div class="mb-2 text-lg font-semibold  text-gold">
+        L’attestato
+      </div>
+      <br>
+      <div class="text-white">
+        ● Attestato di partecipazione in formato PDF, con QR Code e certificazione digitale
+        <br>
+      </div>
+      <br>
+
+      <br>
+      <div class="mb-2 text-lg font-semibold  text-gold">
+        L’iscrizione
+      </div>
+      <br>
+      <div class="text-white">
+        L’iscrizione può essere perfezionata alle seguenti condizioni, compilando l’apposito modulo, reperibile al link https://rebrand.ly/5b6018:
+        <br>
+        <br>
+        ● iscrizione ridotta: € 585,00, mediante pagamento in un’unica soluzione;
+        <br>
+        ● iscrizione standard: € 650,00, mediante pagamento in un’unica soluzione o con dilazione da tre a cinque rate mensili senza interessi.
+        <br>
+        <br>
+        La riduzione è riservata agli ex allievi di AIGES Academy e ai soci di MediaCoor.
+        <br>
+        <br>
+        All’iscrizione perfezionata mediante pagamento in un’unica soluzione sono applicabili i seguenti sconti condizionati:
+        <br>
+        <br>
+        ● 5% per i soci di AIAF Campania;
+        <br>
+        ● 3% per gli iscritti agli ordini professionali.
+        <br>
+        <br>
+        Gli sconti condizionati non sono cumulabili.
+        <br>
+        <br>
+        L'iscrizione comprende i seguenti benefit:
+        <br>
+        <br>
+        ● tre incontri gratuiti di supervisione individuale sul primo caso di coordinazione genitoriale;
+        <br>
+        ● un anno di iscrizione a MediaCoor alla metà del costo.
+        <br>
+        <br>
+        Il termine ultimo per perfezionare l'iscrizione è il 10 maggio 2024, salvo in caso di esaurimento dei posti.
+      </div>
+      <br>
+
+      <br>
+      <div class="mb-2 text-lg font-semibold  text-gold">
+        Riconoscimento e accreditamento
+      </div>
+      <br>
+      <div class="text-white">
+        Il riconoscimento è stato concesso da MediaCoor, secondo gli standard previsti dalle linee guida per la coordinazione genitoriale AFCC 2017-2019.
+        <br>
+        <br>
+        L’accreditamento è stato richiesto al CNF per gli avvocati e i praticanti abilitati e a MediaCoor per i mediatori familiari.
+      </div>
+      <br>
+
+      <br>
+      <div class="mb-2 text-lg font-semibold  text-gold">
+        Recensioni verificate
+      </div>
+      <br>
+      <div class="text-white">
+        Molto ben organizzato e diretto, con una buona combinazione tra teoria e pratica. Il post-vendita è particolarmente utile, garantendo un prezioso affiancamento in supervisione gratuita nella fase iniziale dell’incarico (preparazione all’incontro informativo, verifica dell’incontro informativo e preparazione al primo incontro, verifica del primo incontro e preparazione allo svolgimento del processo). Il tutto, potendo usufruire di strumenti di lavoro condivisi, valido e insostituibile aiuto per chi si cimenta per la prima volta in questo ruolo. (Mara Mazzara)
+        <br>
+        <br>
+        Emozionante e arricchente, tenuto da docenti di grande spessore umano e professionale. Bellissimo! (Raffaella Troiano)
+        <br>
+        <br>
+        Completo e altamente formativo. Nonostante il grande impegno richiesto dalla formula full immersion, avrei voluto continuare a godere della coinvolgente esperienza offerta da Manuela Pagliaroli. (Alessandra Moscato)
+        <br>
+        <br>
+        Un’esperienza che può riassumersi nella frase di J Rice: «Fai oggi quello che gli altri non faranno e ottieni domani quello che gli altri non otterranno». Forte innovazione nei contenuti, grande preparazione dei docenti e una continua interazione capace di generare un proficuo scambio professionale. Lieta di avervi partecipato. (Mariarita Ruccia)
+      </div>
+    </div> -->
+    <ac-separator class="mb-10 mt-20" />
 
     <div class="relative mt-20 h-48  border-neutral-800  text-white">
       <ac-marquee :duration="60" :repeat="4">
