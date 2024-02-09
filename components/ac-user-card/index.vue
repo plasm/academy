10e43a436ea478c7f5db03727a5e116f5674f865
Marco Dell'Anna <marco.dellanna@dmgroup.it>
2024-02-09 18:05:32 +0100

    organizzazione

---
+++ components/ac-user-card/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="space-y-4" data-name="ac-user-card">
+    <div class="aspect-[3/2]">
+      <img class="size-full object-cover" :src="image" alt="">
+    </div>
+    <div class="inline-flex w-full items-start justify-between">
+      <div class="space-y-1">
+        <h3 class="text-lg font-medium leading-6 text-white">
+          {{ title }}
+        </h3>
+        <p class="text-base text-gray-500">
+          {{ role }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ac-user-card',
+  props: {
+    image: {
+      type: String,
+      default: null
+    },
+    title: {
+      type: String,
+      default: null
+    },
+    role: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
