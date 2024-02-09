bcfceccc4ac93ecbb53f23e113ab0eef5b694da5
Marco Dell'Anna <marco.dellanna@dmgroup.it>
2024-02-09 17:53:25 +0100

    page: organizzazione

--- pages/organizzazione.vue
+++ pages/organizzazione.vue
@@ -7,6 +7,54 @@
         </div>
       </template>
     </ac-heading>
+
+    <!--
+
+     -->
+
+    <div class="mb-2 text-lg font-semibold  text-gold">
+      I docenti
+    </div>
+    <br>
+    <div class="text-white">
+      Manuela Pagliaroli, avvocata civilista, mediatrice familiare, scolastica, interculturale, civile e commerciale, coordinatrice genitoriale, formatrice e supervisora in mediazione familiare e coordinazione genitoriale, cofondatrice e direttrice scientifica di AIGES Academy, socia fondatrice e coordinatrice del Comitato scientifico di MediaCoor.
+    </div>
+    <br>
+    <div class="text-white">
+      Arturo Maniaci, avvocato civilista, professore associato di Istituzioni di diritto privato presso l'Università Statale di Milano.
+    </div>
+    <br>
+    <div class="text-white">
+      Sara Trabucchi, avvocata civilista, già consigliera e componente della Commissione famiglia e minori presso il Consiglio dell'Ordine degli Avvocati di Verona.
+    </div>
+    <br>
+    <div class="text-white">
+      Clara Veneto, avvocata penalista, responsabile dell'Osservatorio Scuole territoriali e Specializzazione presso l'Unione Camere Penali Italiane, componente della Commissione Reati in ambito familiare della Camera Penale di Roma.
+    </div>
+    <br>
+    <div class="text-white">
+      Paolo Palumbo, avvocato canonista, professore straordinario di Diritto canonico ed ecclesiastico e di Psicologia forense canonica presso l'Università Giustino Fortunato, docente di Diritto di famiglia presso la Pontificia Facoltà Teologica dell'Italia Meridionale di Napoli, giudice uditore presso il Tribunale Ecclesiastico Interdiocesano e di Appello di Benevento, presidente di sezione dell'Unione Giuristi Cattolici Italiani.
+    </div>
+    <br>
+    <div class="text-white">
+      Alessandra Simonelli, psicologa, psicoterapeuta, professoressa di prima fascia di Psicologia dinamica e Psicopatologia dello sviluppo e direttrice del Dipartimento di Psicologia dello sviluppo e della Socializzazione presso l'Università di Padova.
+    </div>
+    <br>
+    <div class="text-white">
+      Cinzia Vitale, sociologa, criminologa, mediatrice familiare, membro del Consiglio direttivo e del Comitato di esperti del Centro per l'UNESCO di Firenze.
+    </div>
+    <br>
+    <div class="text-white">
+      Tiziana Fragomeni, avvocata negoziatrice, mediatrice, formatrice in tecniche di negoziazione, mediazione e trasformazione dei conflitti, ideatrice del metodo OASI (Osservare, Ascoltare, Sentire, Integrare).
+    </div>
+    <br>
+    <div class="text-white">
+      Isabella Salmeri, mediatrice familiare, coordinatrice genitoriale, formatrice in leadership, intelligenza emotiva, comunicazione strategica e problem-solving.
+    </div>
+
+    <!--
+
+     -->
   </div>
 </template>
 
