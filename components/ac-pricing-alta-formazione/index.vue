f954a0e107633b7ec21b98fe6874c6842ecdcfea
Marco Dell'Anna <marco.dellanna@dmgroup.it>
2024-02-16 14:32:30 +0100

    feat!: replace component

--- components/ac-pricing-alta-formazione/index.vue
+++ components/ac-pricing-alta-formazione/index.vue
@@ -1,75 +1,40 @@
 <template>
-  <section data-name="ac-pricing">
-    <div class="relative mx-auto w-full max-w-5xl items-center px-4 md:px-12 lg:px-16">
-      <div class="mx-auto grid grid-cols-1 items-start lg:grid-cols-2">
-        <section class="flex flex-col px-6 sm:px-8 lg:py-8">
-          <h3 class="mt-5 text-lg uppercase text-blue-200">
-            Iscrizione ridotta
-          </h3>
-          <p class="mt-2 text-sm text-white">
-            Pagamento in un'unica soluzione
-          </p>
-          <div class="mt-2 text-sm text-neutral-400">
-            Agevolazione riservata agli ex allievi di AIGES Academy e ai soci di MediaCoor
-          </div>
-          <p class="order-first text-5xl font-light tracking-tight text-white">
-            € 585,00
-          </p>
-          <ul role="list" class="order-last mt-10 flex flex-col gap-y-3 text-sm text-white">
-            <li class="flex items-center">
-              <span class="ml-4 text-sm"><u>Benefit inclusi</u></span>
-            </li>
-            <li class="flex items-center">
-              <div class="ml-4 text-sm text-neutral-400">
-                <div>Tre incontri gratuiti di supervisione individuale sul primo caso di coordinazione genitoriale</div>
-              </div>
-            </li>
-            <li class="flex items-center">
-              <div class="ml-4 text-sm text-neutral-400">
-                <div>Un anno di iscrizione a MediaCoor alla metà del costo</div>
-              </div>
-            </li>
-          </ul>
-          <a class="nline-flex mt-8 w-full items-center justify-center rounded-full border-2 border-white bg-white px-6 py-2.5 text-center text-sm text-black duration-200 hover:border-white hover:bg-transparent hover:text-white focus:outline-none focus-visible:outline-white focus-visible:ring-white" href="https://rebrand.ly/5b6018">
-            Iscriviti
-          </a>
-        </section>
-        <section class="order-first flex flex-col rounded-3xl border border-neutral-700 bg-gradient-to-br from-neutral-800 to-neutral-900 px-6 py-8 sm:px-8 lg:order-none">
-          <h3 class="mt-5 text-lg uppercase text-blue-200">
-            Iscrizione standard
-          </h3>
-          <p class="mt-2 text-sm text-neutral-100">
-            Pagamento in un'unica soluzione o con dilazione da tre a dieci rate mensili senza interessi
-          </p>
-          <div class="mt-2 text-sm text-neutral-400">
-            Sconto del 5% per i soci di AIAF Campania<br>
-            Sconto del 3% per gli iscritti agli ordini professionali
-          </div>
-          <p class="order-first text-5xl font-light tracking-tight text-white">
-            € 650,00
-          </p>
-          <ul role="list" class="order-last mt-10 flex flex-col gap-y-3 text-sm text-white">
-            <li class="flex items-center">
-              <span class="ml-4 text-sm"><u>Benefit inclusi</u></span>
-            </li>
-            <li class="flex items-center">
-              <div class="ml-4 text-sm text-neutral-400">
-                <div>Tre incontri gratuiti di supervisione individuale sul primo caso di coordinazione genitoriale</div>
-              </div>
-            </li>
-            <li class="flex items-center">
-              <div class="ml-4 text-sm text-neutral-400">
-                <div>Un anno di iscrizione a MediaCoor alla metà del costo</div>
-              </div>
-            </li>
-          </ul>
-          <a class="nline-flex mt-8 w-full items-center justify-center rounded-full border-2 border-blue-200 bg-blue-200 px-6 py-2.5 text-center text-sm text-black duration-200 hover:border-blue-200 hover:bg-transparent hover:text-blue-200 focus:outline-none focus-visible:outline-white focus-visible:ring-white" href="https://rebrand.ly/5b6018">
-            Iscriviti
-          </a>
-        </section>
-      </div>
-    </div>
-  </section>
+  <ac-pricing data-name="ac-pricing-alta-formazione">
+    <template #cards>
+      <ac-pricing-card
+        title="ISCRIZIONE RIDOTTA"
+        subtitle="Pagamento in un'unica soluzione"
+        price="585,00"
+        link="https://rebrand.ly/dff6g2m"
+        theme="silver"
+      >
+        <template #description>
+          Agevolazione riservata agli ex allievi di AIGES Academy e ai soci di MediaCoor
+        </template>
+        <template #footer>
+          <div>Tre incontri gratuiti di supervisione individuale sul primo caso di coordinazione genitoriale</div>
+          <div>Un anno di iscrizione a MediaCoor alla metà del costo</div>
+        </template>
+      </ac-pricing-card>
+      <ac-pricing-card
+        title="ISCRIZIONE STANDARD"
+        subtitle="Pagamento in un'unica soluzione o con dilazione da tre a dieci rate mensili senza interessi"
+        price="650,00"
+        link="https://rebrand.ly/dff6g2m"
+        theme="silver"
+        highlight
+      >
+        <template #description>
+          Sconto del 5% per i soci di AIAF Campania<br>
+          Sconto del 3% per gli iscritti agli ordini professionali
+        </template>
+        <template #footer>
+          <div>Tre incontri gratuiti di supervisione individuale sul primo caso di coordinazione genitoriale</div>
+          <div>Un anno di iscrizione a MediaCoor alla metà del costo</div>
+        </template>
+      </ac-pricing-card>
+    </template>
+  </ac-pricing>
 </template>
 
 <script>
